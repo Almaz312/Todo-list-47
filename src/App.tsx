15e78b2257ec581efcef38773d8bcb8ef56076e0
deadpooll312 <deadpoll312@gmail.com>
2021-11-17 01:07:49 +0600

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,28 @@ import React, {useState} from 'react';
 import './App.css';
 import {TasksType, Todolist} from "./Todolist";
 
+export type FilterValueType = "all" | "completed" | "active";
+
 function App() {
-    let [tasks, setTasks] = useState([
+    let [tasks, setTasks] = useState<Array<TasksType>>([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
         {id: 4, title: "Redux", isDone: false},
     ]);
-    let [filter, setFilter] = useState("all")
+    let [filter, setFilter] = useState<FilterValueType>("all")
 
     function removeTask(id: number) {
         let filteredTasks = tasks.filter(t => t.id !== id);
         setTasks(filteredTasks);
     };
 
+    function changeFilter(value: FilterValueType) {
+        setFilter(value);
+    };
+
     let tasksForTodolist = tasks;
-    if (filter === "Completed") {
+    if (filter === "completed") {
         tasksForTodolist = tasks.filter(t => t.isDone === true);
     }
     if (filter === "active") {
@@ -29,6 +35,7 @@ function App() {
             <Todolist title={"What to learn"}
                       tasks={tasksForTodolist}
                       removeTask={removeTask}
+                      changeFilter={changeFilter}
             />
         </div>
     );
